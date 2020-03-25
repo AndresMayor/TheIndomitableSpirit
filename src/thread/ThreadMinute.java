5bf81441ff6dc2ca77e1dc832fa78a22980f3566
Jhonatan Arboleda <arboledajhonatan03@gmail.com>
2020-03-24 23:42:37 -0500

    Final

--- src/thread/ThreadMinute.java
+++ src/thread/ThreadMinute.java
@@ -5,8 +5,8 @@ public class ThreadMinute  implements Runnable {
 	@Override
 	public void run() {
 		try {
-			//Thread.sleep(180000);
-			Thread.sleep(30000);
+			Thread.sleep(180000);
+//			Thread.sleep(30000);
 		} catch (InterruptedException e) {
 			
 			e.printStackTrace();
